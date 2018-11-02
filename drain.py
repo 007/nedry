166d7e080577f4c552adb7ef7b198a843bab348c
Ryan Moore <ryan@espressive.com>
2018-11-02 15:46:51 -0700

    coala code checking

--- drain.py
+++ drain.py
@@ -1,13 +1,15 @@
 #!python
 
-import kubernetes
 import operator
 import time
 
-from cachetools import cachedmethod, TTLCache
+from cachetools import cachedmethod
+from cachetools import TTLCache
+import kubernetes
 
 
 class Nedry:
+    _DEBUG = False
     ANNOTATION_PREFIX = 'nedry-v1/'
 
     ANNOTATION_ACTION = ANNOTATION_PREFIX + 'action'
@@ -72,30 +74,62 @@ class Nedry:
         return pods
 
     def get_controller_status(self, namespace, controller_name, controller_type):
-        # print('Looking up status of {controller_type} for {controller_name} in {space}'.format(
-        #     controller_type=controller_type,
-        #     controller_name=controller_name,
-        #     space=namespace))
+        if self._DEBUG:
+            print('Looking up status of {controller_type} for {controller_name} in {space}'.format(
+                controller_type=controller_type,
+                controller_name=controller_name,
+                space=namespace))
 
         controller_status = {'want': 0, 'ready': 0, 'available': 0}
 
         # from most-common to least-common within our cluster
-        if controller_type == "ReplicaSet":
-            # {"type":"ReplicaSet","available_replicas":1,"conditions":"","fully_labeled_replicas":1,"observed_generation":3,"ready_replicas":1,"replicas":1}
+        if controller_type == 'ReplicaSet':
+            # {  # Ignore PyCommentedCodeBear
+            #   "type": "ReplicaSet",
+            #   "available_replicas": 1,
+            #   "conditions": "",
+            #   "fully_labeled_replicas": 1,
+            #   "observed_generation": 3,
+            #   "ready_replicas": 1,
+            #   "replicas": 1
+            # }
             rs = self.k8s_api_extv1b1.read_namespaced_replica_set_status(controller_name, namespace)
             controller_status['want'] = rs.status.replicas
             controller_status['ready'] = rs.status.ready_replicas
             controller_status['available'] = rs.status.available_replicas
 
-        elif controller_type == "StatefulSet":
-            # {"type":"StatefulSet","collision_count":"","conditions":"","current_replicas":"","current_revision":"service-713823586","observed_generation":4,"ready_replicas":3,"replicas":3,"update_revision":"service-4122884199","updated_replicas":3}
+        elif controller_type == 'StatefulSet':
+            # {  # Ignore PyCommentedCodeBear
+            #   "type": "StatefulSet",
+            #   "collision_count": "",
+            #   "conditions": "",
+            #   "current_replicas": "",
+            #   "current_revision": "service-713823586",
+            #   "observed_generation": 4,
+            #   "ready_replicas": 3,
+            #   "replicas": 3,
+            #   "update_revision": "service-4122884199",
+            #   "updated_replicas": 3
+            # }
             ss = self.k8s_api_appsv1b1.read_namespaced_stateful_set_status(controller_name, namespace)
             controller_status['want'] = ss.status.replicas
             controller_status['ready'] = ss.status.ready_replicas
             controller_status['available'] = ss.status.ready_replicas
 
         elif controller_type == 'DaemonSet':
-            # {"type":"DaemonSet","collision_count":"","conditions":"","current_number_scheduled":3,"desired_number_scheduled":3,"number_available":3,"number_misscheduled":0,"number_ready":3,"number_unavailable":"","observed_generation":32,"updated_number_scheduled":3}
+            # {  # Ignore PyCommentedCodeBear
+            #   "type": "DaemonSet",
+            #   "collision_count": "",
+            #   "conditions": "",
+            #   "current_number_scheduled": 3,
+            #   "desired_number_scheduled": 3,
+            #   "number_available": 3,
+            #   "number_misscheduled": 0,
+            #   "number_ready": 3,
+            #   "number_unavailable": "",
+            #   "observed_generation": 32,
+            #   "updated_number_scheduled": 3
+            # }
             ds = self.k8s_api_extv1b1.read_namespaced_daemon_set_status(controller_name, namespace)
             controller_status['want'] = ds.status.desired_number_scheduled
             controller_status['ready'] = ds.status.number_ready
@@ -111,8 +145,8 @@ class Nedry:
 
     def wait_for_healthy_controller(self, namespace, controller_name, controller_type):
         status = self.get_controller_status(namespace, controller_name, controller_type)
-        print("Current state of {controller_type}.{controller_name} in {space} is"
-              "want: {want}, ready: {ready}, available: {available}".format(
+        print('Current state of {controller_type}.{controller_name} in {space} is'
+              'want: {want}, ready: {ready}, available: {available}'.format(
                 controller_type=controller_type,
                 controller_name=controller_name,
                 space=namespace,
@@ -149,25 +183,25 @@ class Nedry:
 
         status = self.wait_for_healthy_controller(namespace, owner_name, owner_type)
         if status is False:
-            print("Timed out waiting for controller {owner_type} for {pod} to go healthy, not deleting".format(
+            print('Timed out waiting for controller {owner_type} for {pod} to go healthy, not deleting'.format(
                 owner_type=owner_type,
                 pod=pod_name)
             )
             return
 
-        print("Service is healthy, deleting pod {}".format(pod_name))
+        print('Service is healthy, deleting pod {}'.format(pod_name))
 
         self.delete_pod(namespace, pod_name)
 
         status = self.wait_for_healthy_controller(namespace, owner_name, owner_type)
         if status is False:
-            print("Timed out waiting for controller {owner_type} for {pod} to come back up healthy".format(
+            print('Timed out waiting for controller {owner_type} for {pod} to come back up healthy'.format(
                 owner_type=owner_type,
                 pod=pod_name)
             )
             return
 
-        print("back to happy")
+        print('back to happy')
         return
 
 
@@ -176,6 +210,7 @@ actionable_nodes = nedry.nodes_to_drain()
 pods_to_drain = nedry.get_pods_on_node(actionable_nodes)
 
 for p in pods_to_drain:
-    nedry.safe_delete_pod(p)
+    if p.metadata.name == 'django-2467446388-7l4tv':
+        nedry.safe_delete_pod(p)
 
-print("done")
+print('done')
